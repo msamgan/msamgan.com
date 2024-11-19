40af8a88d6b501b56f7ef6c468e91451a5426746
msamgan <mohdsamgankhan@gmail.com>
2024-11-19 15:40:28 -0500

    added resume.

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -14,20 +14,33 @@ new class extends Component
 
 <div class="">
     @foreach ($data["navigation"]["pages"] as $page)
-        <a
-            href="{{ url($page["link"]) }}"
-            wire:navigate
-            class="@if (request()->is($page["link"]))
-                text-red-700
-            @endif block px-4 py-2 text-black text-opacity-80 hover:text-opacity-100"
-        >
-            <span class="flex flex-row justify-start gap-1">
-                @if (request()->is($page["link"]))
-                    <x-selected-icon />
-                @endif
+        @if ($page["external"])
+            <a
+                href="{{ url($page["link"]) }}"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="block px-4 py-2 text-black text-opacity-80 hover:text-opacity-100"
+            >
+                {{ $page["name"] }}
+            </a>
+        @else
+            <a
+                href="{{ url($page["link"]) }}"
+                wire:navigate
+                class="@if (request()->is($page["link"]))
+                    text-red-700
+                @else
+                    text-black
+                @endif block px-4 py-2 text-opacity-80 hover:text-opacity-100"
+            >
+                <span class="flex flex-row justify-start gap-1">
+                    @if (request()->is($page["link"]))
+                        <x-selected-icon />
+                    @endif
 
-                <span>{{ $page["name"] }}</span>
-            </span>
-        </a>
+                    <span>{{ $page["name"] }}</span>
+                </span>
+            </a>
+        @endif
     @endforeach
 </div>
