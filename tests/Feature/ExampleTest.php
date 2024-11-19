9a8c0affc0ef86a94fcfd7ee6c56da0376bf9651
msamgan <mohdsamgankhan@gmail.com>
2024-11-18 21:24:43 -0500

    PSR

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,11 +1,11 @@
 <?php
 
-it('returns a successful response', function () {
+it('returns a successful response', function (): void {
     $response = $this->get('/');
 
     $response->assertStatus(200);
 });
 
-it('tests that the .env key are same across all .env files.', function () {
+it('tests that the .env key are same across all .env files.', function (): void {
     $this->artisan('env:keys-check --auto-add=none')->assertExitCode(0);
 });
