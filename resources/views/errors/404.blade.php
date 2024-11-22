24e4076674c20d3bacbace15cd41ebebb99b0670
msamgan <mohdsamgankhan@gmail.com>
2024-11-22 16:01:02 -0500

    added 404.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,8 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>404 Error</title>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet" />
-
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
 
