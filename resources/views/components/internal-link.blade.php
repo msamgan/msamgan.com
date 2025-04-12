2b2431b3e77b2ce733715229045b07201eb4925e
msamgan <mohdsamgankhan@gmail.com>
2025-04-12 17:43:04 -0400

    added lact.

--- resources/views/components/internal-link.blade.php
+++ resources/views/components/internal-link.blade.php
@@ -1,17 +1,13 @@
 <a
     href="{{ $link }}"
     wire:navigate
-    class="@if ($active)
-        text-red-700
-    @else
-        text-black
-    @endif block px-4 pb-3 text-opacity-80 hover:text-red-600 hover:text-opacity-100"
+    class="@if ($active) text-red-700 @else text-black @endif block px-4 pb-3 text-opacity-80 hover:text-red-600 hover:text-opacity-100 dark:text-white"
 >
     <span class="flex flex-row justify-start gap-1">
         @if ($active)
             <x-selected-icon />
         @endif
 
-        <span>{{ $page["name"] }}</span>
+        <span>{{ $page['name'] }}</span>
     </span>
 </a>
