94a30b6444074ddad8fdc77da38573c3d7d07574
msamgan <mohdsamgankhan@gmail.com>
2025-04-30 18:56:11 -0400

    added key checker docs.

---
+++ resources/views/components/layouts/docs.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" xmlns:livewire="http://www.w3.org/1999/html">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {{-- add dynamic head elements --}}
+        @if (isset($head))
+            {{ $head }}
+        @else
+            <title>{{ config('app.name') }}</title>
+        @endif
+        <meta name="author" content="Mohammad Samgan Khan" />
+        <meta property="og:url" content="{{ url()->current() }}" />
+        <meta name="og:type" content="website" />
+        <meta name="og:site_name" content="msamgan.com" />
+
+        @if (isset($canonical))
+            {{ $canonical }}
+        @else
+            <link rel="canonical" href="{{ url()->current() }}" />
+        @endif
+
+        <script
+            defer
+            src="https://cloud.umami.is/script.js"
+            data-website-id="5feea10c-6999-43d0-983f-f963a4631506"
+            data-domains="msamgan.com"
+        ></script>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}" />
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}" />
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}" />
+        <link rel="manifest" href="{{ url('/site.webmanifest') }}" />
+        <link rel="mask-icon" href="{{ url('/safari-pinned-tab.svg') }}" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+
+        <!-- Styles / Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+
+    <body class="dark:bg-gray-800">
+        <div class="mx-auto flex max-w-7xl justify-between bg-white dark:bg-gray-800">
+            <div class="w-full">
+                {{ $slot }}
+            </div>
+
+            <livewire:mobilemenu />
+        </div>
+
+        <x-footer />
+    </body>
+</html>
--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -1,3 +1,48 @@
 @php
+    use League\CommonMark\Environment\Environment;
+    use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+    use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkExtension;
+    use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkRenderer;
+    use League\CommonMark\MarkdownConverter;
 
+    $project = request('project');
+    $page = request('page') ?? 'index';
+    $filePath = resource_path('views/docs/' . $project . DIRECTORY_SEPARATOR . $page . '.md');
+
+    if (file_exists($filePath)) {
+        $mdContent = file_get_contents($filePath);
+        // Convert Markdown to HTML
+
+        $config = [
+            'heading_permalink' => [
+                'html_class' => 'heading-permalink',
+                'id_prefix' => '',
+                'apply_id_to_heading' => false,
+                'heading_class' => '',
+                'fragment_prefix' => '',
+                'insert' => 'before',
+                'min_heading_level' => 1,
+                'max_heading_level' => 6,
+                'title' => 'Permalink',
+                'symbol' => HeadingPermalinkRenderer::DEFAULT_SYMBOL,
+                'aria_hidden' => true,
+            ],
+        ];
+
+        $environment = new Environment($config);
+
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new HeadingPermalinkExtension());
+
+        $converter = new MarkdownConverter($environment);
+        $mdContent = $converter->convert($mdContent);
+    } else {
+        $mdContent = '<p>Documentation not found.</p>';
+    }
 @endphp
+
+<x-layouts.docs>
+    <article class="docs post mb-12 space-y-8 text-gray-900">
+        {!! $mdContent !!}
+    </article>
+</x-layouts.docs>
